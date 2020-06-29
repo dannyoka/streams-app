1fcf79068e0e27b64449ba2fa78766ab4c0f6855
Danny Oka <dannyoka@gmail.com>
2020-06-29 12:58:07 -0700

    added editStreams, and switch StreamCreate and StreamEdit to factor in StreamForms

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { Router, Route } from 'react-router-dom'
 import StreamCreate from './streams/StreamCreate'
 import StreamList from './streams/StreamList'
-import {StreamEdit} from './streams/StreamEdit'
+import StreamEdit from './streams/StreamEdit'
 import {StreamDelete} from './streams/StreamDelete'
 import {StreamShow} from './streams/StreamShow'
 import { Header } from './Header'
@@ -18,9 +18,9 @@ export default class App extends Component {
                     <Header />
                     <Route path="/" exact component={StreamList} />
                     <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
-                    <Route path="/streams/delete" exact component={StreamDelete} />
-                    <Route path="/streams/show" exact component={StreamShow} />
+                    <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                    <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                    <Route path="/streams/show/:id" exact component={StreamShow} />
                 </div>
             </Router>
             </div>
